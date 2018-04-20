7c97a2bffceeb7b6ed057fac33133f021adb5b5c
Nikka <dejesusnikka@gmail.com>
2018-04-20 11:03:30 +0800

    Random Tests

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,35 @@
 import javax.swing.JFrame;
-import java.util.Random();
+import java.util.Random;
 import java.util.Scanner;
+import java.lang.*;
 
 public class Main {
 	private static Process[] processes;	
 	static int ctr = 0;
 	static int noOfProcesses = 0;
-	Scanner scan;
-	Random rand = new Random();
-	//burstTime 0-50
-	//noOfProcesses 0-20
+	static int algorithm = 0;
+	
 	public static void main(String[] args) {
-
 		System.out.println("Enter number of processes: ");
-		scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		noOfProcesses = scan.nextInt();
 		
 		while(noOfProcesses > 20){
 			System.out.println("Enter number of processes: ");
-			scan = new Scanner(System.in);
 			noOfProcesses = scan.nextInt();
 		}
 		
-		int algorithms[] = {SchedulingAlgorithm.SRTF};
+		System.out.println("Choose which algorithm to use: ");
+		System.out.println("0 - FCFS\n1 - SJF\n2 - SRTF\n3 - NP_PRIO\n4 - PRIO\n5 - RR\nCHOICE: ");
+		algorithm = scan.nextInt();
+		
+		int algorithms[] = {SchedulingAlgorithm.FCFS};
 		long respectiveQuantum[] = {2000};
 		
 		processes = new Process[noOfProcesses];
-		int burstTime[] = new int[noOfProcesses];
-		int arrivalTime[] = new int[noOfProcesses];
+		long burstTime[] = new long[noOfProcesses];
+		long arrivalTime[] = new long[noOfProcesses];
+		int priority[] = new int[noOfProcesses];
 		
 		GanttChart gantt = new GanttChart();
 		gantt.setVisible(true);
@@ -35,21 +37,45 @@ public class Main {
 		gantt.init(algorithms, respectiveQuantum);
 		
 		Scheduler scheduler = new Scheduler(algorithms.length);	
-		
 		scheduler.generateQueues(algorithms, respectiveQuantum);
 		
+		Random rand = new Random();
+		int tmp1, tmp2;
 		for(int i = 0; i < noOfProcesses; i++){
-			arrivalTime[i] = rand.nextInt((noOfProcesses*50)+1);
-			burstTime[i] = rand.nextInt(51);
-			processes[i] = new Processes(i+1, arrivalTime[i], burstTime[i], 0)
+			tmp1 = rand.nextInt(10000);
+			arrivalTime[i] = new Long(tmp1);
+			tmp2 = rand.nextInt(5000);
+			burstTime[i] = new Long(tmp2);
+		} 
+		
+		if(algorithm == SchedulingAlgorithm.FCFS || algorithm == SchedulingAlgorithm.SJF || algorithm == SchedulingAlgorithm.SRTF || algorithm == SchedulingAlgorithm.RR){
+			System.out.println("PID		Arrival Time 		Burst Time");
+			for(int i = 0; i < noOfProcesses; i++){
+				processes[i] = new Process(i+1, arrivalTime[i], burstTime[i], 0);
+				System.out.println(" " + (i+1) + " 		" + arrivalTime[i] + "			" + burstTime[i]);
+			}
+		}else if(algorithm == SchedulingAlgorithm.NP_PRIO || algorithm == SchedulingAlgorithm.PRIO){
+			System.out.println("PID		Arrival Time 		Burst Time		Priority");
+			for(int i = 0; i < noOfProcesses; i++){
+				priority[i] = rand.nextInt(20)+1;
+				processes[i] = new Process(i+1, arrivalTime[i], burstTime[i], priority[i]);
+				System.out.println(" " + (i+1) + " 		" + arrivalTime[i] + "			" + burstTime[i] + "	" + priority[i]);
+			}
 		}
-		/*
-		processes[0] = new Process(1, 0, 10000, 5);
-		processes[1] = new Process(2, 1000, 2000, 4);
-		processes[2] = new Process(3, 1000, 1000, 1);
-		processes[3] = new Process(4, 5000, 5000, 4);
-		processes[4] = new Process(5, 5000, 3000, 3);
-		*/
+		
+		//sort processes by arrival time
+		Process temp;
+		for(int i = 0; i < noOfProcesses; i++){  
+			for(int j = 1; j < (noOfProcesses - i); j++){  
+				if(arrivalTime[j - 1] > arrivalTime[j]){  
+					//swap elements  
+					temp = processes[j-1];
+					processes[j-1] = processes[j];				
+					processes[j] = temp;
+				}
+			}
+    }
+
 		scheduler.initProcesses(processes);
 		scheduler.simulate();
 	}
